d9b8c863c9b02651baa9f0751b64a805666a74df
IamAshishSingh02 <psssasds212004@gmail.com>
2026-02-21 14:39:05 +0000

    feat(auth-service): initialize auth service with proper middleware order and global error handling

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,35 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from 'cors'
+import {errorMiddleware} from '../../../packages/error-handler/error-middleware'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
-});
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  credentials: true
+}))
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
-});
+app.use(express.json())
+app.use(cookieParser())
+
+// Health Route
+app.get('/auth-health', (req, res) => {
+  res.json({ message: 'Auth-service is running' })
+})
+
+// 404 handler
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found' })
+})
+
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+    console.log(`Auth-service is running at http://localhost:${port}/api`);
+})
+server.on('error', (err) => {
+    console.log('Server Error: ', err);
+})
