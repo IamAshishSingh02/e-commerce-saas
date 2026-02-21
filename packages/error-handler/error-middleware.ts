b233326be7d9991ca39b59a320259f77434e97d7
IamAshishSingh02 <psssasds212004@gmail.com>
2026-02-21 14:28:36 +0000

    feat(error-handler): implement centralized AppError classes and global error middleware

---
+++ packages/error-handler/error-middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import { AppError } from './index'
+
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    console.error(`Error ${req.method} ${req.url} - ${err.message}`)
+
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+      ...(err.details && { details: err.details })
+    })
+  }
+
+  console.error('Unhandled Error:', err)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong'
+  })
+}
