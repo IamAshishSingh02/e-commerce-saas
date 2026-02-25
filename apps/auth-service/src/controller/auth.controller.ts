696fd02171cbf07f08b3ef6a161e9d855809d992
IamAshishSingh02 <psssasds212004@gmail.com>
2026-02-25 19:02:17 +0000

    feat(auth-service): implement login with JWT access/refresh tokens and secure cookie handling

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import {
   checkOtpRestrictions,
   sendOtp,
@@ -8,7 +9,8 @@ import {
   verifyOtp
 } from '../utils/auth.helper'
 import prisma from '@packages/libs/prisma'
-import { ValidationError } from '@packages/error-handler'
+import { AuthError, ValidationError } from '@packages/error-handler'
+import { setCookie } from '../utils/cookies/setCookie'
 
 // Register new user
 export const userRegistration = async (
@@ -77,6 +79,75 @@ export const verifyUser = async (
     return res.status(201).json({
       success: true,
       message: 'User registered successfully!'
+    })
+
+  } catch (error) {
+    return next(error)
+  }
+}
+
+// Login user
+export const loginUser = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  try {
+    const {email, password} = req.body
+    
+    if(!email || !password){
+      return next(new ValidationError('Email and Password are required!'))
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {email}
+    })
+
+    if(!user){
+      return next(new AuthError(`User doesn't exists!`))
+    }
+
+    // Verify password
+    const isMatch = await bcrypt.compare(password, user.password!)
+    if(!isMatch){
+      return next(new AuthError('Invalid email or password!'))
+    }
+
+    // Generate access token
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        role: 'user'
+      }, 
+      process.env.ACCESS_TOKEN_SECRET as string,
+      {
+        expiresIn: '15m'
+      }
+    )
+
+    // Generate refresh token
+    const refreshToken = jwt.sign(
+      {
+        id: user.id,
+        role: 'user'
+      }, 
+      process.env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: '7d'
+      }
+    )
+
+    // Store the tokens in httpOnly secure cookie
+    setCookie(res, 'access_token', accessToken)
+    setCookie(res, 'refresh_token', refreshToken)
+
+    res.status(200).json({
+      message: 'Login successful!',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
     })
 
   } catch (error) {
